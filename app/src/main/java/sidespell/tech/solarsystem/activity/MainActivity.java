3859d1bff7e42686110c4c0ccac2e67ed090bb2a
jeanneviegarciano <jeanne_ctuie89@yahoo.com>
2016-01-07 17:36:17 -0800

    first commit

--- app/src/main/java/sidespell/tech/solarsystem/activity/MainActivity.java
+++ app/src/main/java/sidespell/tech/solarsystem/activity/MainActivity.java
@@ -1,25 +1,55 @@
 package sidespell.tech.solarsystem.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
 import sidespell.tech.solarsystem.R;
+import java.util.ArrayList;
+import java.util.List;
+import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
-public class MainActivity extends AppCompatActivity {
-
-
-
+public class MainActivity extends Activity implements OnItemSelectedListener{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+
+        spinner.setOnItemSelectedListener(this);
+
+        List<String> categories = new ArrayList<String>();
+        categories.add("Mercury");
+        categories.add("Venus");
+        categories.add("Earth");
+        categories.add("Mars");
+        categories.add("Jupiter");
+        categories.add("Saturn");
+        categories.add("Uranus");
+        categories.add("Neptune");
+        categories.add("Pluto");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        String item = parent.getItemAtPosition(position).toString();
+
+
+       // Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+    public void onNothingSelected(AdapterView<?> arg0) {
+
     }
 }
